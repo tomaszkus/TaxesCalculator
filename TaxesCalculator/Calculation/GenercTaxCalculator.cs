10230e92f165306a7fca0958929bef2b7e486ce6
Tomasz Kus <tkus@future-processing.com>
2016-04-27 12:59:22 +0200

    Implementation

--- TaxesCalculator/Calculation/GenercTaxCalculator.cs
+++ TaxesCalculator/Calculation/GenercTaxCalculator.cs
@@ -9,12 +9,34 @@
         public virtual Money CalculateFor(Money incomes, bool shouldBeProgresssive)
         {
 
-            if (incomes.Amount < 0)
+
+            if (shouldBeProgresssive)
             {
-                throw new ArgumentOutOfRangeException();
+                if (incomes.Amount < 0)
+                {
+                    throw new ArgumentOutOfRangeException();;
+                }
+
+                return new Money(incomes.Amount * 0.2m);
+            }
+            else
+            {
+                if (incomes.Amount < 40000 && incomes.Amount >=0) {
+                    return new Money(incomes.Amount * 0.15m);
+                }
+                else if (incomes.Amount > 40000 && incomes.Amount < 80000)
+                {
+                    return new Money(40000 * 0.15m + (incomes.Amount - 40000) * 0.30m);;}
+                else if (incomes.Amount > 40000 && incomes.Amount < 80000)
+                {
+                    return new Money(40000 * 0.15m + (incomes.Amount - 40000) * 0.45m);
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
             }
 
-            return new Money(incomes.Amount * 0.2m);
         }
     }
 }
--- TaxesCalculatorTests/FlatTaxCalculatorTest.cs
+++ TaxesCalculatorTests/FlatTaxCalculatorTest.cs
@@ -42,5 +42,49 @@
             // then
             Assert.That(result.Amount, Is.EqualTo(expectedTax));
         }
+
+        [Test]
+        public void ShouldCalculateProgresssiveTax1_1()
+        {
+            var result = calculator.CalculateFor(new Money(5000), false);
+            Assert.IsTrue(result.Amount == 750);
+        }
+
+
+        [Test]
+        public void shouldCalculateProgresssiveTax1_2()
+        {
+            var result = calculator.CalculateFor(new Money(20000), false);
+            Assert.IsTrue(result.Amount == 3000);
+        }
+
+        [Test]
+        public void ShoudlCalculateProgresssiveTax1_3()
+        {
+            var result = calculator.CalculateFor(new Money(35000), false);
+            Assert.IsTrue(result.Amount == 5250);
+        }
+
+        [Test]
+        public void ShouldCalculateProgresssiveTax2_1()
+        {
+            var result = calculator.CalculateFor(new Money(45000), false);
+            Assert.IsTrue(result.Amount == 7500);
+        }
+
+
+        [Test]
+        public void shouldCalculateProgresssiveTax2_2()
+        {
+            var result = calculator.CalculateFor(new Money(55000), false);
+            Assert.IsTrue(result.Amount == 10500);
+        }
+
+        [Test]
+        public void ShoudlCalculateProgresssiveTax2_3()
+        {
+            var result = calculator.CalculateFor(new Money(65000), false);
+            Assert.IsTrue(result.Amount == 13500);
+        }
     }
 }
